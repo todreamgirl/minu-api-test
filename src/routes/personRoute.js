82af614e10b68949779e3b157d5fe8ee8ebbd384
unknown <jacquelinee.barbosa@hotmail.com>
2021-05-03 10:57:22 -0300

    update-crud

--- src/routes/personRoute.js
+++ src/routes/personRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/personController')
-router.post('/', controller.post);
-router.put('/:id', controller.put);
-router.delete('/:id', controller.delete);
+router.post('/usuario', UsuarioController.post);
+router.put('/usuario/:id', UsuarioController.put);
+router.delete('/usuario/:id', UsuarioController.delete);
+router.get('/usuarios', UsuarioController.get);
+router.get('/usuario/:id', UsuarioController.getById);
 module.exports = router;
+
+
